765a2ed913d6f875b1c35bd9c7707a3025793e5c
Rajkumar Kalita <rajkumarriyan001@gmail.com>
2023-04-24 23:02:11 +0530

    chnaged proxy url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   app.use(
     "/",
     createProxyMiddleware({
-      target: "https://pizza-khao.cyclic.app",
+      target: "https://cute-pink-squid-tutu.cyclic.app",
       changeOrigin: true,
     })
   );
