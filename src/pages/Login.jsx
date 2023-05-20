c073d4a16655f1a03a5a994d6e7a7c98bc730084
Rajkumar Kalita <rajkumarriyan001@gmail.com>
2023-05-20 10:34:29 +0530

    desgin: added button disable function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,25 +10,40 @@ const Login = () => {
   const { loading, error, isAuthenticated } = useSelector(
     (state) => state.user
   );
-  const [phone, setPhone] = useState(null);
+  const [contact, setContact] = useState("");
   const [password, setPassword] = useState("");
   const [otpLoader, setOtpLoader] = useState(false);
+  const [isOTPButtonDisabled, setIsOTPButtonDisabled] = useState(true);
+  const [isLoginButtonDisabled, setLoginButtonDisabled] = useState(true);
+  const userNumber = Number.parseInt(contact);
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(login(phone, password));
+    dispatch(login(userNumber, password));
   };
   const handleLoginUsingOtp = async (e) => {
-    e.preventDefault();
     setOtpLoader(true);
-    const { data } = await axios.post(`/api/login/contact`, {
-      contact: phone,
-    });
-
-    if (data.success) {
-      navigate(`/verify/login/otp`, { state: { phone } });
+    try {
+      const { data } = await axios.post(`/api/login/contact`, {
+        userNumber,
+      });
+      if (data.success) {
+        navigate(`/verify/login/otp`, { state: { userNumber } });
+      }
+    } catch (error) {
+      toaster.error(error.response.data.message);
+    } finally {
+      setOtpLoader(false);
     }
-    setOtpLoader(false);
   };
+
+  useEffect(() => {
+    // Check if the input value is empty or the length is not equal to 10
+    const isInputEmpty = contact.trim().length === 0;
+    const isPasswordEmpty = password.trim().length == 0;
+    const isInvalidLength = contact.length !== 10;
+    setIsOTPButtonDisabled(isInputEmpty || isInvalidLength);
+    setLoginButtonDisabled(isInputEmpty || isPasswordEmpty || isInvalidLength);
+  }, [contact, password]);
 
   useEffect(() => {
     if (error) {
@@ -45,7 +60,7 @@ const Login = () => {
       <div className="backdrop-blur-md w-max form flex items-center justify-center flex-col gap-10 rounded-lg p-5 sm:p-10">
         <div className="flex items-end gap-1">
           <h1 className="text-xl sm:text-3xl font-extrabold text-white">
-            Welcome Back Mate !
+            Welcome Back Mate!
           </h1>
         </div>
         <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
@@ -53,10 +68,10 @@ const Login = () => {
             <input
               type="number"
               placeholder="Contact number*"
-              className="w-full h-full bg-transparent px-2 text-white"
+              className="w-full h-full bg-transparent px-2 text-white placeholder:font-light"
               autoComplete="off"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
             />
           </div>
 
@@ -64,7 +79,7 @@ const Login = () => {
             <input
               type="password"
               placeholder="Password*"
-              className="w-full h-full bg-transparent px-2 text-white"
+              className="w-full h-full bg-transparent px-2 text-white placeholder:font-light"
               autoComplete="off"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -76,26 +91,42 @@ const Login = () => {
               Logging in...
             </div>
           ) : (
-            <div className="w-full h-10 sm:h-12 bg-red-700 cursor-pointer rounded text-xs sm:text-base overflow-hidden hover:bg-red-800 shadow-inner shadow-red-800">
+            <div
+              className={`w-full h-11 ${
+                isLoginButtonDisabled
+                  ? "bg-red-400 shadow-red-500"
+                  : "bg-red-600 hover:bg-red-700 shadow-red-800"
+              } cursor-pointer rounded text-xs sm:text-base shadow-inner overflow-hidden`}
+            >
               <input
                 type="submit"
                 value="Login"
                 className="h-full w-full uppercase text-white font-medium tracking-wider cursor-pointer sm:text-base text-sm"
+                disabled={isLoginButtonDisabled}
               />
             </div>
           )}
           {otpLoader ? (
-            <div className="w-full h-10 sm:h-12 bg-red-500 text-white cursor-pointer rounded-md text-sm uppercase flex items-center justify-center gap-2 overflow-hidden shadow-inner">
-              <i className="fa-duotone fa-loader fa-spin-pulse text-xl"></i>
+            <div
+              className={`w-full h-10 sm:h-12 bg-blue-500 text-white cursor-pointer rounded-md text-sm uppercase flex items-center justify-center gap-2 overflow-hidden shadow-inner`}
+            >
+              <i className="fas fa-spinner fa-spin text-xl"></i>
               Loading...
             </div>
           ) : (
-            <div className="w-full h-10 sm:h-12 bg-blue-600 cursor-pointer rounded overflow-hidden hover:bg-blue-800 shadow-inner shadow-blue-800">
+            <div
+              className={`w-full h-11 ${
+                isOTPButtonDisabled
+                  ? "bg-blue-400 shadow-blue-500"
+                  : "bg-blue-600  hover:bg-blue-800 shadow-blue-800"
+              } cursor-pointer rounded overflow-hidden shadow-inner `}
+            >
               <input
                 type="submit"
                 value="Request OTP"
                 className="w-full h-full text-white uppercase tracking-wider font-medium cursor-pointer text-sm sm:text-base"
                 onClick={handleLoginUsingOtp}
+                disabled={isOTPButtonDisabled}
               />
             </div>
           )}
@@ -103,9 +134,9 @@ const Login = () => {
             <span className="font-light">Don't have an account? </span>
             <Link
               to="/register"
-              className="font-light text-yellow-300 cursor-pointer hover:text-yellow-600"
+              className="font-light cursor-pointer hover:text-yellow-600 underline text-yellow-400"
             >
-              Register now
+              Create now
             </Link>
           </div>
           <Link
