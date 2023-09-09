2ed80d1c87ba758f57eb4ab6575221804d6d9501
Rajkumar Kalita <rajkumarriyan001@gmail.com>
2023-09-09 22:37:02 +0530

    style: change ui

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,133 +1,139 @@
 import React from "react";
 import { pizzaSize } from "../utils";
-import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { removeCartItem, updateCart } from "../redux/actions/cartActions";
 import { addRemoveFromWishlist } from "../redux/actions/wishListAction";
-import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
 import { useMediaQuery } from "@mui/material";
-const CartItem = ({ item, wishlist }) => {
-  const dispatch = useDispatch();
-  const handleRemoveProduct = (id) => {
-    dispatch(removeCartItem(id));
-  };
-  const isSmallScreen = useMediaQuery("(max-width: 640px)");
-  const moveToWishlist = (id) => {
-    dispatch(addRemoveFromWishlist(id));
-  };
-  const isItemInWishlist = (id) => {
-    return wishlist?.items?.find((item) => item.product._id === id);
-  };
-  const handleSizeChange = (id, quantity, size) => {
-    dispatch(updateCart(id, quantity, size));
-  };
-  return (
-    <div
-      className="flex flex-col h-[8rem] lg:h-52 md:h-44 border-2 p-2 lg:p-5 md:p-3 rounded"
-      key={item.product._id}
-    >
-      <div className="flex items-start lg:gap-6 md:gap-2 h-full">
-        <div className="flex flex-col items-center justify-between h-full lg:gap-4 relative">
-          <div className="h-20 w-20 lg:h-28 lg:w-28 md:h-24 md:w-24 rounded-md overflow-hidden drop-shadow-md">
-            <img
-              src={item?.product?.image}
-              alt={item?.product?.name}
-              className="h-full w-full object-cover"
-            />
-          </div>
-          <div className="flex items-center justify-between gap-2 sm:gap-4">
-            <select
-              className="capitalize border-[1px] border-gray-500 sm:p-1 text-gray-600 rounded-sm font-light cursor-pointer text-xs sm:text-base bg-transparent"
-              value={item.size}
-              onChange={(e) =>
-                handleSizeChange(
-                  item.product._id,
-                  item.quantity,
-                  e.target.value
-                )
-              }
-            >
-              {pizzaSize.map((size, i) => (
-                <option
-                  value={size}
-                  key={i}
-                  className="capitalize font-light cursor-pointer md:text-sm lg:text-base"
-                >
-                  {size}
-                </option>
-              ))}
-            </select>
-            <div className="flex gap-1 border-[1px] border-gray-500 rounded-sm sm:py-1 overflow-hidden bg-transparent">
-              <select
-                value={item.quantity}
-                onChange={(e) =>
-                  handleSizeChange(item.product._id, e.target.value, item.size)
-                }
-                className="cursor-pointer text-xs sm:text-base bg-transparent"
-              >
-                {Array.from(Array(10).keys()).map((x, i) => (
-                  <option
-                    key={i}
-                    value={i + 1}
-                    className="cursor-pointer hover:bg-red-600"
-                  >
-                    {i + 1}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <button
-            onClick={() => moveToWishlist(item?.product._id)}
-            className="absolute -left-1 -top-0 bg-white h-6 md:h-8 w-6 md:w-8 flex items-center justify-center rounded-full"
-          >
-            <i
-              className={`${
-                isItemInWishlist(item?.product._id)
-                  ? "fas fa-heart text-red-600"
-                  : "far fa-heart"
-              } text-sm md:text-base lg:text-lg`}
-            ></i>
-          </button>
-        </div>
-        <div className="flex items-start justify-between w-full h-full">
-          <div className="flex flex-col sm:gap-1">
-            <span className="text-gray-700 font-medium uppercase tracking-wider text-sm sm:text-base">
-              {isSmallScreen
-                ? item.product.name.length > 15
-                  ? `${item.product.name.slice(0, 12)}...`
-                  : item.product.name
-                : item.product.name}
-            </span>
-            {item && item.product && item.product.prices && (
-              <span className="text-golden font-medium text-sm sm:text-lg">
-                ₹{item.product.prices[item.size]}
-              </span>
-            )}
-            <span className="capitalize text-gray-600 text-xs sm:text-sm font-light">
-              {item.size}
-            </span>
-            <span className="text-gray-600 text-xs sm:text-sm font-light">
-              Quantity: {item.quantity}
-            </span>
-          </div>
-          <div className="flex flex-col items-end justify-between h-full">
-            {item?.product?.prices && (
-              <div className="text-red-600 text-base md:text-xl font-semibold">
-                ₹{item.quantity * item.product.prices[item.size]}
-              </div>
-            )}
+const CartItem = ({ item }) => {
+    const dispatch = useDispatch();
+    const { wishlist } = useSelector((state) => state.wishlist);
+    const handleRemoveProduct = (id) => {
+        dispatch(removeCartItem(id));
+    };
+    const isSmallScreen = useMediaQuery("(max-width: 640px)");
+    const moveToWishlist = (id) => {
+        dispatch(addRemoveFromWishlist(id));
+    };
+    const isItemInWishlist = (id) => {
+        return wishlist?.items?.find((item) => item.product._id === id);
+    };
+    const handleSizeChange = (id, quantity, size) => {
+        dispatch(updateCart(id, quantity, size));
+    };
+    return (
+        <div className="bg-white rounded-lg w-60 h-max flex flex-col gap-4 overflow-hidden px-6 py-2 relative drop-shadow">
             <button
-              className="text-xs sm:text-base uppercase text-blue-600 font-normal rounded cursor-pointer"
-              onClick={() => handleRemoveProduct(item.product._id)}
+                onClick={() => moveToWishlist(item?.product._id)}
+                className="absolute top-3 left-3 h-7 w-7 bg-neutral-50 shadow-md flex items-center justify-center rounded-full cursor-pointer"
             >
-              Remove
+                <i
+                    className={` ${
+                        isItemInWishlist(item?.product?._id)
+                            ? "fas text-red-600"
+                            : "fal"
+                    } fa-heart`}
+                ></i>
             </button>
-          </div>
+            <button
+                onClick={() => handleRemoveProduct(item.product._id)}
+                className="absolute top-3 right-3"
+            >
+                <i className="fal fa-trash-alt text-red-700 text-lg"></i>
+            </button>
+            <div className="w-full h-32">
+                <img
+                    src={item.product?.image}
+                    alt={item.product?.name}
+                    className="h-full w-full object-contain"
+                />
+            </div>
+            <div>
+                <h1 className="text-lg font-medium capitalize text-gray-800">
+                    {item.product?.name}
+                </h1>
+                <p className="text-sm text-gray-600 font-light">
+                    {item.product?.description?.substring(0, 35)}...
+                </p>
+                <h1 className="text-red-600 font-medium">
+                    ₹{item.product?.prices?.[item.size]}
+                </h1>
+                <div className="flex gap-6">
+                    <p className="flex items-center gap-1 text-gray-600 text-sm">
+                        <span>Size:</span>
+                        <span className="capitalize">{item.size}</span>
+                    </p>
+                    <p className="flex items-center gap-1 text-gray-600 text-sm">
+                        <span>Quantity: </span>
+                        <span>{item.quantity}</span>
+                    </p>
+                </div>
+                <div className="flex gap-1 text-sm">
+                    <span className="text-gray-600">Subtotal:</span>
+                    <span className="text-red-700 font-medium">
+                        ₹{item.quantity * item.product?.prices?.[item.size]}
+                    </span>
+                </div>
+                <div className="flex items-center justify-between gap-2 mt-2">
+                    <select
+                        value={item.size}
+                        onChange={(e) =>
+                            handleSizeChange(
+                                item.product._id,
+                                item.quantity,
+                                e.target.value
+                            )
+                        }
+                        className="capitalize text-gray-600 w-full border border-gray-400 rounded-md h-8"
+                    >
+                        {pizzaSize.map((size) => (
+                            <option
+                                value={size}
+                                key={size}
+                                onClick={() =>
+                                    handleSizeChange(
+                                        item.product._id,
+                                        item.quantity,
+                                        size
+                                    )
+                                }
+                            >
+                                {size}
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        value={item?.quantity}
+                        className="text-gray-600 w-full border border-gray-400 rounded-md h-8"
+                        onChange={(e) =>
+                            handleSizeChange(
+                                item.product._id,
+                                item.quantity,
+                                e.target.value
+                            )
+                        }
+                    >
+                        {Array.from({ length: 10 }, (_, i) => i + 1).map(
+                            (quantity) => (
+                                <option
+                                    value={quantity}
+                                    key={quantity}
+                                    onClick={() =>
+                                        handleSizeChange(
+                                            item.product._id,
+                                            quantity,
+                                            item.size
+                                        )
+                                    }
+                                >
+                                    {quantity}
+                                </option>
+                            )
+                        )}
+                    </select>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CartItem;
